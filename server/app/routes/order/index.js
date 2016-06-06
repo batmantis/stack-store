784c3a945e9990df804693043d3f927c604fa6fc
batmantis <batmantisex@gmail.com>
2016-06-06 18:44:11 -0400

    added more routes

--- server/app/routes/order/index.js
+++ server/app/routes/order/index.js
@@ -1,9 +1,11 @@
 var router = require('express').Router()
 var db = require('../../../db')
 var Order = db.model('order')
+var User = db.model('user')
 module.exports = router
 
-router.get('/', function(req, res) {
+//Get all orders
+router.get('/', function(req, res, next) {
   Order.findAll({where: req.query})
   .then(function(orders) {
     res.send(orders)
@@ -11,6 +13,20 @@ router.get('/', function(req, res) {
   .catch(next)
 })
 
-router.get('/:orderId', function(req, res) {
+//Get one order by id
+router.get('/:orderId', function(req, res, next) {
+  Order.findById({where: {id: req.params.orderId}})
+  .then(function(order) {
+    res.send(order)
+  })
+  .catch(next)
+})
 
+//Create new order
+router.post('/', function(req, res) {
+  Order.create(req.body)
+  .then(function(order) {
+    res.send(order)
+  })
+  .catch(next)
 })
