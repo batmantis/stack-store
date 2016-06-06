a027569b1efefa265bf247282c631f0ba8014e94
Jansen <jansenli89@gmail.com>
2016-06-06 18:11:24 -0400

    more seeding

--- seed.js
+++ seed.js
@@ -23,6 +23,10 @@ name in the environment files.
 var chalk = require('chalk');
 var db = require('./server/db');
 var User = db.model('user');
+var Product = db.model('product');
+var Tag = db.model('tag');
+var Order = db.model('order');
+var Review = db.model('review');
 var Promise = require('sequelize').Promise;
 
 var seedUsers = function () {
@@ -46,9 +50,95 @@ var seedUsers = function () {
 
 };
 
+var seedProducts = function() {
+    var products = [
+        {
+            name: 'Sock',
+            imageUrl: 'https://s-media-cache-ak0.pinimg.com/736x/fd/56/55/fd5655d3f79a5000b17852c3965e916c.jpg',
+            price: 10.99,
+            brand: 'OneSockMan',
+            description: 'Selfies XOXO gluten-free synth, man braid meggings DIY vinyl echo park pabst. Cornhole art party blue bottle kogi normcore pickled, roof party chillwave beard venmo. Jean shorts organic kale chips, vinyl listicle meggings butcher green juice selvage scenester vegan hella. Kickstarter hoodie meggings banh mi gluten-free. Viral butcher farm-to-table etsy 8-bit. Squid man braid viral ennui. Seitan keffiyeh craft beer cred blog, kinfolk fixie.',
+            quantity: 100
+        },
+        {
+            name: 'North Face Glove',
+            imageUrl: 'http://www.sportsauthority.com/graphics/product_images/pTSA-15061572p275w.jpg',
+            price: 50.99,
+            brand: 'North Face',
+            description: 'Waistcoat swag art party 3 wolf moon, chicharrones seitan stumptown beard church-key chillwave literally sriracha. Poutine four dollar toast paleo cornhole, semiotics lumbersexual cold-pressed vice meditation pour-over whatever helvetica wayfarers yuccie. Beard vinyl man braid, tilde scenester cold-pressed narwhal vice street art hoodie raw denim etsy freegan. Shabby chic disrupt heirloom, church-key banh mi pinterest tattooed. Mustache cardigan brooklyn, bicycle rights dreamcatcher actually readymade leggings asymmetrical four loko selfies you probably haven\'t heard of them XOXO umami. VHS sartorial hoodie fixie, meh letterpress direct trade health goth retro neutra raw denim pickled distillery. Bespoke tilde kale chips lo-fi blog pabst cardigan four dollar toast ugh, seitan cray.',
+            quantity: 100
+        }
+    ]
+
+    var creatingProducts = products.map(function(productObj){
+        return Product.create(productObj);
+    })
+
+    return Promise.all(creatingProducts);
+}
+
+var seedTags = function(){
+    var tags = [
+        {
+            category: 'clothing'
+        },
+        {
+            category: 'food'
+        },
+        {
+            category: 'miscellaneous'
+        }
+    ]
+
+    var creatingTags = tags.map(function(tagObj){
+        return Tag.create(tagObj)
+    });
+
+    return Promise.all(creatingTags)
+}
+
+var seedOrders = function(){
+    var orders = [
+        {
+            address: '2439 Star Route',
+            city: 'Arlington Heights',
+            state: 'IL',
+            zipcode: 60005,
+            billingaddress: '2439 Star Route',
+            billingcity: 'Arlington Heights',
+            billingstate: 'IL',
+            billingzipcode: 60005,
+            creditCard: 5319869385480010,
+            orderTotal: 61.98,
+            orderStatus: 'Created'
+        }
+    ]
+
+    var creatingOrders = orders.map(function(orderObj){
+        return Order.create(orderObj)
+    })
+
+    return Promise.all(creatingOrders)
+}
+
+var seedReviews = function() {
+    var reviews = [
+        {
+            rating: 4,
+            review: 'Etsy gluten-free ethical man bun, banjo actually cray brooklyn plaid farm-to-table. Pug vice farm-to-table etsy ugh humblebrag, echo park gastropub man bun slow-carb pitchfork ramps kitsch lo-fi YOLO. Kitsch affogato tote bag tousled, mlkshk blue bottle yr leggings tacos umami VHS. Freegan put a bird on it listicle, chia offal before they sold out church-key. Literally chartreuse XOXO brooklyn, waistcoat slow-carb squid pour-over retro distillery franzen yr paleo. Blog biodiesel kogi, shabby chic keytar tilde kinfolk chillwave helvetica +1 ugh. Typewriter humblebrag kale chips drinking vinegar seitan mumblecore flannel beard, tilde +1 man braid scenester occupy hammock.'
+        }
+    ]
+
+    var creatingReviews = reviews.map(function(reviewObj){
+        return Review.create(reviewObj)
+    })
+
+    return Promise.all(creatingReviews)
+}
+
 db.sync({ force: true })
     .then(function () {
-        return seedUsers();
+        return Promise.all([seedUsers(), seedProducts(), seedTags(), seedOrders()]);
     })
     .then(function () {
         console.log(chalk.green('Seed successful!'));
