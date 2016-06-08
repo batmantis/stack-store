9f2488aa64582d3ac2b5c81c57b2208ba7f770c3
Jansen <jansenli89@gmail.com>
2016-06-08 18:34:15 -0400

    reviews done

--- browser/js/common/controller/product/product.controller.js
+++ browser/js/common/controller/product/product.controller.js
@@ -1,4 +1,39 @@
-app.controller('productController', function($scope, product, $state){
-	$scope.product = product
+app.controller('productController', function(productFactory, $rootScope, $scope, product, $state, AuthService, Session, reviewFactory) {
+    $scope.product = product
 
+    // $scope.review = {}
+
+    // $scope.addReview = function(review, productId){
+    // 	reviewFactory.addReview(review, productId)
+    // 	.then(function(){
+    // 		return productFactory.getProductDetails(productId)
+    // 		.then(function(product){
+    // 			$scope.product = product
+    // 		})
+    // 	})
+    // }
+
+    $scope.addReview = function(review, productId) {
+        reviewFactory.addReview(review, productId)
+            .then(function(review) {
+            	console.log($scope.product.reviews)
+                $scope.product.reviews.unshift(review)
+            })
+    }
+
+    // $scope.user = null;
+
+    $scope.user = Session.user;
+
+    // $scope.isLoggedIn = function () {
+    //     return AuthService.isAuthenticated();
+    // };
+
+    // var setUser = function () {
+    //     AuthService.getLoggedInUser().then(function (user) {
+    //         $scope.user = user;
+    //     });
+    // };
+
+    // setUser();
 })
---
+++ browser/js/common/controller/review/review.controller.js
@@ -0,0 +1,3 @@
+app.controller('reviewCtrl', function($scope, reviewFactory){
+	$scope.addReview = reviewFactory.addReview
+});
