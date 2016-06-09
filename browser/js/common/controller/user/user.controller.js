894b9e8e2252b95b138306ef12cc8decfea104d2
Jansen <jansenli89@gmail.com>
2016-06-09 18:17:19 -0400

    delet address and save address

--- browser/js/common/controller/user/user.controller.js
+++ browser/js/common/controller/user/user.controller.js
@@ -1,7 +1,19 @@
-app.controller('userDetailController', function ($scope, $log, $state, loggedInUser) {
+app.controller('userDetailController', function ($scope, $log, $state, loggedInUser, addressFactory) {
 	if (loggedInUser) {
 		$scope.user = loggedInUser;
 	} else {
 		$state.go('notLoggedIn');
+	}
+
+	$scope.delete = function(addressId) {
+		addressFactory.deleteAddress(addressId)
+		.then(function(){
+			$scope.$evalAsync();
+		})
+	}
+
+	$scope.update = function() {
+		console.log('i went here')
+		$scope.$evalAsync();
 	}
 });
--- browser/js/common/directives/user/address.directive.js
+++ browser/js/common/directives/user/address.directive.js
@@ -4,8 +4,11 @@ app.directive('addAddress', function(addressFactory) {
         // name: '',
         // priority: 1,
         // terminal: true,
+        scope: {
+        	update: '&'
+        },
         // scope: {}, // {} = isolate, true = child, false/undefined = no change
-        // controller: function($scope, $element, $attrs, $transclude) {},
+        // controller: 'userDetailController',
         // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
         restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
         templateUrl: 'js/common/directives/user/addressform.html',
@@ -31,6 +34,8 @@ app.directive('addAddress', function(addressFactory) {
                     .then(function() {
                         scope.reset();
                         scope.addressForm.$setPristine()
+                        // scope.$apply();
+                        scope.update();
                     })
             }
 
