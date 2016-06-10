089b3b2e1c55f702d860a7b467c87f8cac40d6c7
Jansen <jansenli89@gmail.com>
2016-06-09 21:06:23 -0400

    fixed the address directive with updating the dom/scope

--- browser/js/common/directives/user/address.directive.js
+++ browser/js/common/directives/user/address.directive.js
@@ -10,14 +10,14 @@ app.directive('addAddress', function(addressFactory) {
         // scope: {}, // {} = isolate, true = child, false/undefined = no change
         // controller: 'userDetailController',
         // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-        restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+        restrict: 'EA', // E = Element, A = Attribute, C = Class, M = Comment
         templateUrl: 'js/common/directives/user/addressform.html',
         // templateUrl: '',
         // replace: true,
-        // transclude: true,
+        transclude: true,
         // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
         link: function(scope) {
-            scope.defaultForm = {
+            var defaultForm = {
                 name: "",
                 address: "",
                 city: "",
@@ -26,16 +26,18 @@ app.directive('addAddress', function(addressFactory) {
             }
 
             scope.reset = function() {
-                scope.form = scope.defaultForm
+                scope.form = defaultForm
             }
 
             scope.save = function() {
                 addressFactory.saveAddress(scope.form)
                     .then(function() {
+                    	scope.update()
                         scope.reset();
                         scope.addressForm.$setPristine()
                         // scope.$apply();
-                        scope.update();
+                        
+                    // }).then(function(){
                     })
             }
 
