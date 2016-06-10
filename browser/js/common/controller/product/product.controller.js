65ec92b7d371420b8ee709a19c546bc5056cd47f
fonzilla <j.alfonso.antonio@gmail.com>
2016-06-10 15:43:12 -0400

    qty dropdown

--- browser/js/common/controller/product/product.controller.js
+++ browser/js/common/controller/product/product.controller.js
@@ -1,15 +1,27 @@
 app.controller('productController', function(productFactory, $scope, product, $state, Session, reviewFactory, CartFactory) {
-    $scope.product = product
+    $scope.product = product;
 
     $scope.addReview = function(review, productId) {
         reviewFactory.addReview(review, productId)
             .then(function(review) {
                 $scope.product.reviews.unshift(review)
             })
+    };
+
+    $scope.number = 10;
+
+
+    $scope.getNumber = function(num){
+        var amount = +this.product.quantity;
+        if(amount < 10){
+            return new Array(amount);
+        }
+        return new Array(num);
     }
 
-    $scope.addToCart = CartFactory.addToCart
+    $scope.addToCart = CartFactory.addToCart;
 
     $scope.user = Session.user;
+
 
 })
